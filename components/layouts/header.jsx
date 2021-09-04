1a9695a60b97a6e8c61f3cc6dc457fc203aba018
ahmadridhoni <ahmadridhoni@outlook.co.id>
2021-09-04 19:37:20 +0700

    update base layout

--- components/layouts/header.jsx
+++ components/layouts/header.jsx
@@ -1,16 +1,34 @@
 import * as Hi from 'react-icons/hi'
+import Logo from '../brand/logo'
 
 export default function Header() {
     return(
-        <header>
-            <nav className="fixed px-10 bg-black text-white h-20 w-full flex justify-between place-items-center">
+        <header className="select-none">
+            <nav className="fixed px-3 md:px-10 bg-black text-white h-20 w-full flex justify-between place-items-center">
                 <div>
-                    <h1>Ahmadridhoni</h1>
+                    <Logo />
                 </div>
                 <div>
-                    <button className="text-3xl">
+                    <button className="hidden sm:block text-2xl">
                         <Hi.HiMenuAlt3 />
                     </button>
+                    <button className="sm:hidden text-2xl">
+                        <Hi.HiOutlineMoon />
+                    </button>
+                </div>
+            </nav>
+            <nav className="fixed bottom-0 px-10 text-xl bg-black text-white h-20 w-full sm:hidden flex justify-between place-items-center">
+                <div aria-describedby="home" role="button">
+                    <Hi.HiHome />
+                </div>
+                <div aria-describedby="home" role="button">
+                    <Hi.HiOutlineHome />
+                </div>
+                <div aria-describedby="home" role="button">
+                    <Hi.HiOutlineHome />
+                </div>
+                <div aria-describedby="home" role="button">
+                    <Hi.HiOutlineHome />
                 </div>
             </nav>
         </header>
