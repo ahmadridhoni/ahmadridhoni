bb7e61eb9efacd0f0a015db192ce57aa8ab2aec2
ahmadridhoni <ahmadridhoni@outlook.co.id>
2021-09-04 17:41:01 +0700

    initial project

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function About() {
+    return (
+        <div>
+            about
+            <Link href="/"><a>hello</a></Link>
+        </div>
+    )
+}
